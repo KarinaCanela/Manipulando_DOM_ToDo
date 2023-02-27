cc917be050d055c44e24ed26ce107b8739e0b588
karina <kcanelacobos@gmail.com>
2023-02-27 13:39:32 -0600

    clase 05 completada en donde se elimina la tarea

--- script.js
+++ script.js
@@ -19,31 +19,41 @@ const createTask = (evento) => {
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-
+    
     //task.innerHTML = content;
    
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
-}    
+};    
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask)
+btn.addEventListener('click', createTask);
 
 const checkComplete = () => {
     const i = document.createElement('i');
     i.classList.add('far', 'fa-check-square', 'icon');
     i.addEventListener('click', completeTask);
     return i;
-}
+};
 
 const completeTask = (event) => {
     const element = event.target;
     element.classList.toggle('fas');
     element.classList.toggle('completeIcon');
     element.classList.toggle('far');
+};
+
+const deleteIcon = () => {
+    const i = document.createElement("i")
+    i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    i.addEventListener("click", deleteTask)
+    return i;
+}
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
 }
 
 }) ()
